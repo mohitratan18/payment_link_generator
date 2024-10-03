bb30ccfc898d13ecd0cfb485fa1059484eca4cb1
Mohit Ratan <107945852+mohitratan18@users.noreply.github.com>
2024-10-03 15:36:09 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,6 @@
 const { nextui } = require("@nextui-org/theme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    content: ["./src/**/*.html"],
-    safelist: [
-      "max-w-[390px]",
-      "sm:max-w-[640px]",
-      "sm:min-h-[720px]",
-      "sm:min-w-[640px]",
-      "drop-shadow-xl",
-      "max-w-[180px]",
-      "bg-blue-600",
-      "hover:bg-blue-400",
-    ],
-  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
