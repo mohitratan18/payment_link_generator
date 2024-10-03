486432314a21f278cdaef31c04c3e21f93c8ed7b
Mohit Ratan <107945852+mohitratan18@users.noreply.github.com>
2024-10-03 15:30:41 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,37 @@
-const {nextui} = require('@nextui-org/theme');
+const { nextui } = require("@nextui-org/theme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: {
+    content: ["./src/**/*.html"],
+    safelist: [
+      "max-w-[390px]",
+      "sm:max-w-[640px]",
+      "sm:min-h-[720px]",
+      "bg-white",
+      "sm:min-w-[640px]",
+      "flex",
+      "flex-col",
+      "p-4",
+      "sm:p-5",
+      "gap-8",
+      "drop-shadow-xl",
+      "text-sm",
+      "sm:text-lg",
+      "font-medium",
+      "max-w-[180px]",
+      "bg-blue-600",
+      "p-4",
+      "text-white",
+      "text-lg",
+      "rounded-full",
+      "hover:bg-blue-400",
+    ],
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
